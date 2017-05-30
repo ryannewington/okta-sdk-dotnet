abf3c5278be7ebe3460516e9bccc425ba7c32b8d
Nate <nathanaelb@gmail.com>
2017-05-30 16:37:30 -0700

    Cleanup

--- src/Okta.Sdk.UnitTests/DefaultSerializerShould.cs
+++ src/Okta.Sdk.UnitTests/DefaultSerializerShould.cs
@@ -41,23 +41,6 @@ namespace Okta.Sdk.UnitTests
             (dict["baz"] as IReadOnlyDictionary<string, object>)["qux"].Should().Be(123L);
         }
 
-//        [Fact]
-//        public void DeserializeWithCaseInsensitiveKeys()
-//        {
-//            var json = @"
-//{
-//  ""foo"": ""bar"",
-//  ""baz"": 123
-//}";
-
-//            var serializer = new DefaultSerializer();
-//            var dict = serializer.Deserialize(json);
-
-//            dict["foo"].Should().Be("bar");
-//            dict["FOO"].Should().Be("bar");
-//            dict["BaZ"].Should().Be(123L);
-//        }
-
         [Fact]
         public void DeserializeNullInput()
         {
