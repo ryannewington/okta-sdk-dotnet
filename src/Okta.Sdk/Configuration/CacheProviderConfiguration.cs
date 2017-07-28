4f99de59650b5734f3abc128d4cc4877a41437e6
Nate Barbettini <nathanaelb@gmail.com>
2017-07-28 12:27:30 -0700

    WIP

---
+++ src/Okta.Sdk/Configuration/CacheProviderConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Distributed;
+using Okta.Sdk.Internal;
+
+namespace Okta.Sdk.Configuration
+{
+    public sealed class CacheProviderConfiguration : IDeepCloneable<CacheProviderConfiguration>
+    {
+        public IDistributedCache Provider { get; set; }
+
+        // TODO: timespan?
+        public int? DefaultTtl { get; set; } = 300; // Seconds
+
+        public int? DefaultTti { get; set; } = 300; // Seconds
+
+        public CacheProviderConfiguration DeepClone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
